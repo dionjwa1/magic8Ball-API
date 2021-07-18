f9a1822f7738310d7a5cdb2a111b4de6901e6245
dionjwa1 <dionjwa@gmail.com>
2021-07-17 21:13:59 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require ('express');
+const cors = require ('cors');
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server is Running'));
