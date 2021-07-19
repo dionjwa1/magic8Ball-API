f9cd919a693236778732c90890ec19c60ba5129b
dionjwa1 <dionjwa@gmail.com>
2021-07-18 17:47:42 -0700

    got server running and get route

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+// 'use strict';
+// // const express = require ('express');
+
+// app.get('/data', (req, res) => {
+//     // should respond with a 'Person"
+//     res.send('Hey, you hit my get route!');
+//   });
+  
+//   module.exports = app;
